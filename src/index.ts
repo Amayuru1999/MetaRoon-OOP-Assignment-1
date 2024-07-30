212c08298b01c653ad2ffa0791432e29f816bed2
Amayuru1999 <amarasingheau@gmail.com>
2024-07-30 11:47:20 +0530

    feat:created LibraryCatalogue instances in index.ts #1

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,10 @@ const book1=new Book(1,"Madol Duwa","Martin Wickramasinghe","9789552000000");
 const book2=new Book(2,"Giraya","Martin Wickramasinghe","9789552000001");
 
 const libraryCatalogue = new LibraryCatalogue();
+
+//Adding items to the library
 libraryCatalogue.addItem(book1);
 libraryCatalogue.addItem(book2);
+
+//Displaying items in the library
+libraryCatalogue.displayItems();
