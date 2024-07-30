03806c48238e41c09002af22488710b88c1528bb
Amayuru1999 <amarasingheau@gmail.com>
2024-07-30 11:49:19 +0530

    feat:created User instances in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {Book} from "./Book";
 import {LibraryCatalogue} from "./LibraryCatalogue";
+import {User} from "./User";
 
 
 const book1=new Book(1,"Madol Duwa","Martin Wickramasinghe","9789552000000");
@@ -13,3 +14,8 @@ libraryCatalogue.addItem(book2);
 
 //Displaying items in the library
 libraryCatalogue.displayItems();
+
+//creating a new user
+const user = new User("Amayuru", "amarasingheau@gmail.com");
+user.borrowBook("Madol Duwa");
+user.manageLibrary()
