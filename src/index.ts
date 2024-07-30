65d4597fed17988efa176df7e3ab02fcf0fa407d
Amayuru1999 <amarasingheau@gmail.com>
2024-07-30 11:35:09 +0530

    feat:created instances from the Book class

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import {Book} from "./Book";
+
+
+const book1=new Book(1,"Madol Duwa","Martin Wickramasinghe","9789552000000");
+const book2=new Book(2,"Giraya","Martin Wickramasinghe","9789552000001");
+
